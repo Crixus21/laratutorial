c592b98b174ff959d6aa834f685c4ea572e9b2ad
Antal Laszlo <antal.laszlo.net@gmail.com>
2025-01-27 20:52:40 +0100

    Routing and view practice

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,40 @@
 <?php
 
+use Faker\Provider\HtmlLorem;
+use Faker\Provider\Lorem;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorem dignissimos a sit tenetur recusandae accusamus minima nesciunt nihil, iure molestias! Ab optio rem aut maxime consectetur! Odit, inventore id!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorem dignissimos a sit tenetur recusandae accusamus minima nesciunt nihil, iure molestias! Ab optio rem aut maxime consectetur! Odit, inventore id!'
+            ]
+        ]
+    ]);
 });
 
-
+/*
 Route::get('/hello', function(){
-    response("Hello world");
+    return response("<h1>Hello world</h1>")
+    ->header('Content-type', 'text/plain')
+    ->header('foo', 'bar');
 });
+
+Route::get('/posts/{id}', function($id){
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+    return $request->name . ' ' . $request->city;
+});
+*/
